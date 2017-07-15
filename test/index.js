8ad3b1593f31e28734be326e8d6d6cf457d739fe
zkochan <zoltan.kochan@gmail.com>
2017-07-15 23:28:57 +0300

    fix: use supi instead of pnpm

--- test/index.js
+++ test/index.js
@@ -1,21 +1,23 @@
 'use strict'
 const test = require('tape')
 const removeAllExceptOuterLinks = require('..')
-const pnpm = require('pnpm')
+const supi = require('supi')
 const path = require('path')
 const exists = require('path-exists')
+const rimraf = require('rimraf-then')
 
 const fixtureDir = path.join(__dirname, 'fixture')
 const modules = path.join(fixtureDir, 'node_modules')
 
 test('remove all from a node_modules external symlinks', t => {
-  pnpm.install({cwd: fixtureDir})
-    .then(() => pnpm.link(path.join(__dirname, '..'), fixtureDir))
-    .then(() => pnpm.link(path.join(__dirname, 'scoped-pkg'), fixtureDir))
-    .then(() => pnpm.install({prefix: fixtureDir}))
+  rimraf(modules)
+    .then(() => supi.install({cwd: fixtureDir}))
+    .then(() => supi.link(path.join(__dirname, '..'), fixtureDir))
+    .then(() => supi.link(path.join(__dirname, 'scoped-pkg'), fixtureDir))
+    .then(() => supi.install({prefix: fixtureDir}))
     .then(() => removeAllExceptOuterLinks(modules))
     .then(() => {
-      t.ok(exists.sync(path.join(modules, 'is-negative')))
+      t.ok(!exists.sync(path.join(modules, 'is-negative')))
       t.ok(!exists.sync(path.join(modules, '@types')))
       t.ok(!exists.sync(path.join(modules, '.modules.yaml')))
       t.ok(!exists.sync(path.join(modules, '.shrinkwrap.yaml')))
